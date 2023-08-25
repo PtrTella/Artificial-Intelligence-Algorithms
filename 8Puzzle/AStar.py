e8d1e0b70e58d0b99af2924bffa095b9025debdf
Tella <tellarinipietro@gmail.com>
2023-08-25 18:38:13 +0200

    update bug in AStar.py

--- 8Puzzle/AStar.py
+++ 8Puzzle/AStar.py
@@ -1,11 +1,31 @@
 from queue import PriorityQueue
 import time
 
-
-mode = {
-    "m": "MANHATTAN",
-    "w": "WRONG_POSITION",
+# Dictionary with the possible moves and the opposite of the move
+possibleMoves = {
+        'up': (-3, 'down'),
+        'down': (3, 'up'),
+        'left': (-1, 'right'),
+        'right': (1, 'left')
 }
+
+# Calculate the Manhattan distance based on [1, 2, 3, 4, 5, 6, 7, 8, 0]
+def manhattan_distance(state):
+    distance = 0
+    for i, tile in enumerate(state):
+        if tile != 0:
+            x, y = divmod(i, 3)
+            x_goal, y_goal = divmod(tile-1, 3)
+            distance += abs(x - x_goal) + abs(y - y_goal)
+    return distance
+
+# Calculate the number of tiles in the wrong position based on [1, 2, 3, 4, 5, 6, 7, 8, 0]
+def wrong_position(state):
+    distance = 0
+    for i, tile in enumerate(state):
+        if tile != 0 and tile != i+1:
+            distance += 1
+    return distance
 
 
 class Node:
@@ -18,34 +38,31 @@ class Node:
 
     def __lt__(self, other):
         return self.cost < other.cost
+    
 
 
 class Puzzle:
 
-    possibleMoves = {
-        'up': (-3, 'down'),
-        'down': (3, 'up'),
-        'left': (-1, 'right'),
-        'right': (1, 'left')
-    }
-
-    def __init__(self, initial_state):
-        self.initial_state = initial_state
+    def __init__(self, initial_state, mode="w"):
+        self.mode = mode
+        self.start_node = Node(initial_state, None, None, 0, 0)
         self.goal_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
+    # Generate the children nodes of the given node
     def get_children(self, node):
-
+        
         children = []
         # Find the position of the blank tile (0)
         blank_pos = node.state.index(0)
 
-        # Generate the possible moves
-        for move, (offset, opposite) in self.possibleMoves.items():
+        # Generate the possible moves given by the dictionary possibleMoves
+        for move, (offset, opposite) in possibleMoves.items():
 
             new_pos = blank_pos + offset     # Calculate the position of the tile to be moved
 
-            # Check if it is not the opposite of the last move and if it is not the first move
-            if (opposite != node.move or node.depth == 0):
+            # Check if it is not the opposite of the last move (to avoid the same moves in a loop)
+            # If it is the first Node, the last move is None so it is not necessary to check (opposite != None)
+            if (opposite != node.move):
 
                 # Check if the move is valid (inside vector 0-8) and if it is not the opposite of the last move
                 if (move == "up") and (new_pos < 0):
@@ -58,55 +75,40 @@ class Puzzle:
                     continue
 
                 # Create a new state by swapping the blank tile with the tile to be moved
-                new_state = node.state[:]
-                new_state[blank_pos], new_state[new_pos] = new_state[new_pos], new_state[blank_pos]
+                new_state = node.state[:]  # Copy the current state
+                new_state[blank_pos], new_state[new_pos] = new_state[new_pos], new_state[blank_pos] # Swap the tiles
 
-                # Calculate the heuristic
+                # Calculate the heuristic value with the selected mode
                 h = self.heuristic(new_state)
 
                 # Create a new node and add it to the children list
                 child_node = Node(new_state, node, move,
                                     node.depth+1, node.depth+1+h)
-                children.append(child_node)
-
-                
+                children.append(child_node)    
         return children
 
     # select Heuristic function
     def heuristic(self, state):
-        if mode == "m":                             # mode == "MANHATTAN"               
+        if self.mode == "m":                            # mode == "MANHATTAN"               
             return self.manhattan_distance(state)
-        else:                                       # mode == "WRONG_POSITION"
+        else:                                           # mode == "WRONG_POSITION"
             return self.wrong_position(state)
         
 
-    # Calculate the Manhattan distance
-    def manhattan_distance(self, state):
-        distance = 0
-        for i, tile in enumerate(state):
-            if tile != 0:
-                x, y = divmod(i, 3)
-                x_goal, y_goal = divmod(tile-1, 3)
-                distance += abs(x - x_goal) + abs(y - y_goal)
-        return distance
-
-    # Calculate the number of tiles in the wrong position
-    def wrong_position(self, state):
-        distance = 0
-        for i, tile in enumerate(state):
-            if tile != 0 and tile != i+1:
-                distance += 1
-        return distance
-
+    # Solve the puzzle
     def solve(self):
-        start_node = Node(self.initial_state, None, None, 0, 0)
+        
+        # Using a priority queue to store the nodes is faster to get the node with the lowest cost
         pq = PriorityQueue()
-        pq.put(start_node)
+        pq.put(self.start_node)
+
+        # Using a set to store the visited states is faster than using a list when need to check if a state is visited
         visited = set()
-        moves = []
+        
         while not pq.empty():
             node = pq.get()
             if node.state == self.goal_state:
+                moves = []
                 while node.parent is not None:
                     moves.append(node.move)
                     node = node.parent
@@ -115,20 +117,27 @@ class Puzzle:
             for child in self.get_children(node):
                 if tuple(child.state) not in visited:
                     pq.put(child)
-        return None
+        return [("No solution found")]
 
 
 if __name__ == '__main__':
 
     # Read the initial state from the user
-    initial_state = input("Enter the initial state: ")
-    initial_state = initial_state.strip()
+    initial_state = input("Enter the initial state with spaces: ")
+    initial_state = initial_state.strip().split()
+    
+    # Check if the input is valid
     try:
-        initial_state = initial_state.split()
         if len(initial_state) != 9:
             raise ValueError
+        else :
+            check = [int(i) for i in range(9)]
+            for i in initial_state:
+                if int(i) not in check:
+                    raise ValueError
+                else :
+                    check.remove(int(i))
         initial_state = [int(i) for i in initial_state]
-        puzzle = Puzzle(initial_state)        
     except:
         print("Invalid input")
         exit()
@@ -136,9 +145,10 @@ if __name__ == '__main__':
 
     # Read the mode from the user
     mode = input("Enter the mode (m for Manhattan distance, w for wrong position) [default w]: ")
-    mode = mode.lower()
-    mode = mode.strip()
+    mode = mode.lower().strip()
 
+    puzzle = Puzzle(initial_state, mode)        
+    
     print("\nSolving puzzle...")
     start = time.time()
     moves = puzzle.solve()
